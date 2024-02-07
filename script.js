3c6048fa1fc378706990eef4186364ff2691c55b
Sean Gibbons-Lawrie <s.gibbons745@gmail.com>
2024-02-07 14:43:16 +1300

    marking and unmarking tiles

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
 //Display
-import { createBoard } from './minesweeper.js'
+import { createBoard, markTile, TILE_STATUSES } from './minesweeper.js'
 
 const BOARD_SIZE = 10
 const NUMBER_OF_MINES = 6
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector('.board')
+const minesLeftText = document.querySelector('[data-mine-count]')
 
 console.log(board)
 
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.element)
+    tile.element.addEventListener('click', () => {})
+    tile.element.addEventListener('contextmenu', (e) => {
+      e.preventDefault()
+      markTile(tile)
+      listMinesLeft()
+    })
   })
 })
 boardElement.style.setProperty('--size', BOARD_SIZE)
 
-//1. Populate a board with tiles/mines
-//2. Left click on tiles
+minesLeftText.textContent = NUMBER_OF_MINES
+
+function listMinesLeft() {
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter((tile) => tile.status === TILE_STATUSES.MARKED).length
+    )
+  }, 0)
+
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+}
+
+//1. Populate a board with tiles/mines tick
+//2. Left click on tiles tick
 //a. Reveal Tiles
-//3. Right click on tiles
+//3. Right click on tiles tick
 //a. Mark tiles
 //4. Check for win/lose
